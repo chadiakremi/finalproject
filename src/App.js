a198aa9afd1bc5830759bde036416633c2fbe851
chadiakremi <126797752+chadiakremi@users.noreply.github.com>
2023-04-11 16:59:37 +0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
+import Footer from './Footer';
 import PrimarySearchAppBar from './PrimarySearchAppBar';
-
-
+import SocialLinks from'./SocialLinks';
+import HeroDataGrid from './HeroDatagrid';
 function App() {
   return (
     <div>
      
     <PrimarySearchAppBar/>
-
+    <HeroDataGrid/>
+    <Footer/>
+    <SocialLinks/>
     </div>
   );
 }
