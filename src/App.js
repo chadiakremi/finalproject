babcfd8dcdc3e9718f273eb0d32537d4d93d0b6f
chadiakremi <126797752+chadiakremi@users.noreply.github.com>
2023-04-11 12:41:34 +0400

    first commit
    npm installs must be done manually on the device:
    node_modules
    npx create-react-app
    npm install mui libraries...

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import PrimarySearchAppBar from './PrimarySearchAppBar';
+
+
+function App() {
+  return (
+    <div>
+     
+    <PrimarySearchAppBar/>
+
+    </div>
+  );
+}
+
+export default App;
